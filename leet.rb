5f46280a29ed00bb8e02c072ed5e05dda09a8534
njguld95038043 <t.tamura6060@gmail.com>
2022-01-26 10:44:07 +0000

    [Finish]

--- leet.rb
+++ leet.rb
@@ -1,23 +1,27 @@
-input_line = gets
-strings = input_line.to_s.split('')
-convert = []
-
-strings.each do |chara|
+def convert_leet(chara)
   case chara
   when "A" then
-    convert.push("4")
+    "4"
   when "E" then
-    convert.push("3")
+    "3"
   when "G" then
-    convert.push("6")
+    "6"
   when "I" then
-    convert.push("1")
+    "1"
   when "O" then
-    convert.push("0")
+    "0"
   else
-    convert.push(chara)
+    chara
   end
 end
 
-output  = convert.join
+input = gets
+strings = input.to_s.split('')
+
+convert = []
+strings.each do |chara|
+  convert.push(convert_leet(chara))
+end
+
+output = convert.join
 puts output
